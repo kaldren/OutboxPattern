c3d9042e9150190b5a495ef3b65c55a4120539e9
Kaloyan Drenski <drenski666@gmail.com>
2024-05-29 11:38:43 +0300

    Record

--- OrderingService/Orders/OrderOutbox.cs
+++ OrderingService/Orders/OrderOutbox.cs
@@ -1,12 +1,13 @@
 ﻿namespace OrderingService.Orders;
 
-public class OrderOutbox
-{
-    public int Id { get; set; }
-    public int AggregateId { get; set; }
-    public string AggregateType { get; set; }
-    public string EventType { get; set; }
-    public string EventPayload { get; set; }
-    public DateTimeOffset DateTimeOffset { get; set; }
-    public bool Processed { get; set; }
-}
+public record OrderOutbox
+(
+    int Id,
+    int AggregateId,
+    string AggregateType,
+    string EventType,
+    string EventPayload,
+    DateTimeOffset DateTimeOffset,
+    bool Processed
+);
+
